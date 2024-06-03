efa0864b05a60737028547058da83403da628557
AkinOlu21 <kenneth.akinola@outlook.com>
2024-06-03 16:40:42 +0100

    Navbar development

---
+++ src/assets/assets.js
@@ -0,0 +1,9 @@
+import Pharmafleet_logo from './Pharma fleet logo.jpg'
+import search_icon from './search.svg'
+import basket_icon from './basket.svg'
+
+export const assets = {
+    Pharmafleet_logo,
+    search_icon,
+    basket_icon
+}
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
+import { assets } from '../../assets/assets'
 
 
 const Navbar = () => {
+
+  const [menu, setMenu] = useState("home")
+
   return (
     <div className='navbar' >
-        Navbar
+    <img src={assets.Pharmafleet_logo} alt="" className="logo" />
+    <ul className="nav-menu">
+        <li onClick={()=> setMenu("home")} className={menu=== "home"?"active": ""}>Home</li>
+        <li onClick={()=> setMenu("menu")} className={menu=== "menu"?"active": ""}>Menu</li>
+        <li onClick={()=> setMenu("mobile app")} className={menu=== "mobile app"?"active": ""}>Mobile App</li>
+        <li onClick={()=> setMenu("contact us")} className={menu=== "contact us"?"active": ""}>Contact us</li>
+    </ul>
+
+    <div className="nav-right">
+       <img src={assets.search_icon} alt='search'/>
+    
+    <div className="nav-search-icon">
+        <img src={assets.basket_icon} alt='basket'/>
+        <div className="dot"></div>
+        </div> 
+        <button>Sign in</button>
+    </div>
         
         </div>
   )

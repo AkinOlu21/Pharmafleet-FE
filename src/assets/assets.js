1e257630252a91ab76a2a64def922a91fa60e4b2
AkinOlu21 <kenneth.akinola@outlook.com>
2024-06-04 13:10:41 +0100

    Homepage header

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,9 +1,11 @@
 import Pharmafleet_logo from './Pharma fleet logo.jpg'
 import search_icon from './search.svg'
 import basket_icon from './basket.svg'
+import header_bg from './Delivery header image.webp'
 
 export const assets = {
     Pharmafleet_logo,
     search_icon,
-    basket_icon
+    basket_icon,
+    header_bg
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='Header'>
+<div className="h-cont">
+    <h2>Health is wealth Order from us Today</h2>
+    <p className='.text-primary'>For your convenience PharmaFleet is here to provide</p>
+    <button>Our products</button>
+</div>
+    </div>
+  )
+}
+
+export default Header

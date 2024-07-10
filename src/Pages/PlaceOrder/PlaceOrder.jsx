2e4a520577ba0300429482185bb5abb2f003922e
AkinOlu21 <kenneth.akinola@outlook.com>
2024-07-10 13:28:27 +0100

    Integrated stripe place order functionality

--- src/Pages/PlaceOrder/PlaceOrder.jsx
+++ src/Pages/PlaceOrder/PlaceOrder.jsx
@@ -1,30 +1,89 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import './PlaceOrder.css'
 import { PharmaContext } from '../../Context/PharmaContext'
+import axios from 'axios'
 
 const PlaceOrder = () => {
 
-const {getTotalCartAmount} = useContext(PharmaContext)
+const {getTotalCartAmount,token,product_type,cartItems,url} = useContext(PharmaContext)
+
+const [data,setData] = useState({
+  firstName:"",
+  lastName:"",
+  email:"",
+  address:"",
+  city:"",
+  county:"",
+  country:"",
+  postcode:"",
+  phone:""
+})
+
+  const onChangeHandler = (event) =>{
+    const name = event.target.name;
+    const value = event.target.value;
+    setData(data=>({...data,[name]:value}))
+  }
+
+  useEffect (() => {
+    console.log(data);
+  },[data])
+
+  //creating user place order feature
+  const placeOrder = async (event) =>{
+    event.preventDefault();
+    let orderItems = [];
+    product_type.map((item)=>{
+      if (cartItems[item._id]>0) {
+        let itemInfo = item;
+        itemInfo["quantity"] = cartItems[item._id];
+        orderItems.push(itemInfo);
+      }    
+      
+    })
+    console.log(orderItems);//generated order data
+    let orderData = {
+      address:data,
+      items:orderItems,
+      amount:getTotalCartAmount()+2.5,
+    }
+
+    //sent the order data to the order api
+    let response = await axios.post(url+"/api/order/place",orderData,{headers:{token}});
+
+    //checking if response from order api is true    
+    if (response.data.success) {
+      //user is sent to the session url for stripe payment checkout
+      const {session_url} = response.data;
+      window.location.replace(session_url);
+    } 
+    else {
+      alert("an error occured")
+    }
+    
+    
+
+  }
 
   return (
-    <form className='place-order'>
+    <form onSubmit={placeOrder} className='place-order'>
       <div className="place-order-left">
         <p className="title">Delivery Information</p>
         <div className="multi-fields">
-          <input type="text" placeholder='First name' />
-          <input type="text" placeholder='Last  name' />
+          <input required name='firstName' onChange={onChangeHandler} value={data.firstName} type='text' placeholder='First name' />
+          <input required name='lastName' onChange={onChangeHandler} value={data.lastName} type='text' placeholder='Last  name' />
         </div>
-        <input type="email" placeholder='Email address' />
-        <input type="text" placeholder='Address'/>
+        <input required name='email' onChange={onChangeHandler} value={data.email} type="email" placeholder='Email address' />
+        <input required name='address' onChange={onChangeHandler} value={data.address} type="text" placeholder='Address'/>
         <div className="multi-fields">
-          <input type="text" placeholder='City' />
-          <input type="text" placeholder='County' />
+          <input required name='city' onChange={onChangeHandler} value={data.city} type="text" placeholder='City' />
+          <input required name='county' onChange={onChangeHandler} value={data.county} type="text" placeholder='County' />
         </div>
         <div className="multi-fields">
-          <input type="text" placeholder='PostCode' />
-          <input type="text" placeholder='Country' />
+          <input required name='postcode' onChange={onChangeHandler} value={data.postcode} type="text" placeholder='PostCode' />
+          <input required name='country' onChange={onChangeHandler} value={data.country} type="text" placeholder='Country' />
         </div>
-        <input type="text" placeholder='Mobile number' />
+        <input required name='phone' onChange={onChangeHandler} value={data.phone} type="text" placeholder='Mobile number' />
       </div>
       <div className="place-order-right">
       <div className="cart-total">
@@ -37,16 +96,16 @@ const {getTotalCartAmount} = useContext(PharmaContext)
             <hr />
             <div className="cart-total-details">
               <p>Delivery Fee</p>
-              <p>£{2}</p>
+              <p>£{2.5}</p>
             </div>
             <hr />
             <div className="cart-total-details">
               <b>Total</b>
-              <b>£{getTotalCartAmount()+2}</b>
+              <b>£{getTotalCartAmount()+2.5}</b>
             </div>   
              
           </div>            
-          <button >Proceed to Payment</button>
+          <button type='submit'>Proceed to Payment</button>
       
         </div>
       </div>
